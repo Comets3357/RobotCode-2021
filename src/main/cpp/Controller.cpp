a2f38df26355d91e1c6bf31b4bd209d3eb92d353
Sahil Sawant <sahilsawant2134@gmail.com>
2021-06-06 11:33:52 -0400

    🔨 Update button indices (#1)
    Updated indices to match the Xbox mapping. @tprankprakma

--- src/main/cpp/Controller.cpp
+++ src/main/cpp/Controller.cpp
@@ -80,16 +80,20 @@ void Controller::updateData(RobotData &robotData){
     //secondary controls // indexes are NOT all right yet
     //robotData.sLCenterBtn = getAxis(1, 8); //only used within controller class
     robotData.sDPad = getPOV(1, 0); //secondaryPOVArrayInput in auto
+
     robotData.sLYStick = getAxis(1, 1); //turret control in auto
     robotData.sRYStick = getAxis(1, 5); //turret control in auto
-    robotData.sXBtn = getButton(1, 1); //controls omni wheel indexer in auto
-    robotData.sYBtn = getButton(1, 5); // controls indexer in auto
-    robotData.sLBumper = getButton(1, 3); // controls  intake in auto
-    robotData.sRBumper = getButton (1, 2); // controls intake in auto
-    robotData.sABtn = getButton(1, 4); // controls intake in auto
-    robotData.sRTrigger = getAxis(1, 2); //manual
-    robotData.sLTrigger = getAxis(1, 3); // manual
-    robotData.sLBumper = getButton(1, 6); // manual
+
+    robotData.sLTrigger = getAxis(1, 2); //manual
+    robotData.sRTrigger = getAxis(1, 3); // manual
+
+
+    robotData.sABtn = getButton(1, 1); //controls omni wheel indexer in auto
+    robotData.sBBtn = getButton (1, 2); // controls intake in auto
+    robotData.sXBtn = getButton(1, 3); // controls  intake in auto
+    robotData.sYBtn = getButton(1, 4); // controls intake in auto
+    robotData.sLBumper = getButton(1, 5); // controls indexer in auto
+    robotData.sRBumper = getButton(1, 6); // manual
     
 
 
